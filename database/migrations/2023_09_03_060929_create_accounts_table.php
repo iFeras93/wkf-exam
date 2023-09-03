a2e0cae9fc71026369e131d877bbcd917f86b9a1
Feras Mohamed <ferasmo7mad@gmail.com>
2023-09-03 09:18:14 +0300

    preparing models and migrations

--- database/migrations/2023_09_03_060929_create_accounts_table.php
+++ database/migrations/2023_09_03_060929_create_accounts_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->unsignedBigInteger('user_id');
+            $table->string('number');
+            $table->string('currency');
+            $table->decimal('balance');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('accounts');
     }
 };
--- database/migrations/2023_09_03_060937_create_transactions_table.php
+++ database/migrations/2023_09_03_060937_create_transactions_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedBigInteger('account_id');
+            $table->enum('type', ['credit', 'debit']);
+            $table->decimal('amount');
+            $table->dateTime('timestamp');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
